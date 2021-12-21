4f8d71871566adb885e68d6dabcf06509700ce98
Suqatri <paul@suqatri.net>
2021-12-21 21:07:03 +0100

    Add own onlineServiceVersions.json

--- simplecloud-api/src/main/kotlin/eu/thesimplecloud/api/service/version/loader/ServiceVersionWebLoader.kt
+++ simplecloud-api/src/main/kotlin/eu/thesimplecloud/api/service/version/loader/ServiceVersionWebLoader.kt
@@ -41,7 +41,7 @@ class ServiceVersionWebLoader : IServiceVersionLoader {
 
     override fun loadVersions(): List<ServiceVersion> {
         val version = this::class.java.`package`.implementationVersion.substring(0, 3)
-        val contentString = WebContentLoader().loadContent("https://api.thesimplecloud.eu/versions?implementationVersion[\$lte]=$version")
+        val contentString = WebContentLoader().loadContent("https://cloud.suqatri.net/$version/onlineServiceVersions.json")
         return if (contentString == null) {
             loadFromFile()
         } else {
